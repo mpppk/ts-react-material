54103abdb68f1baff7a290c345b7e7028c56c506
mpppk <niboshiporipori@gmail.com>
2018-04-23 21:29:15 +0900

    Update setting file for webpack4

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,10 @@
 const webpack = require('webpack');
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: [
-    "react-hot-loader/patch",
     "./src/index.tsx"
-    ],
-
-  // Enable sourcemaps for debugging webpack's output.
-  devtool: "cheap-module-eval-source-map",
+  ],
 
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
@@ -20,25 +16,9 @@ module.exports = {
       // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
       { test: /\.tsx?$/,
         loader: [
-          "react-hot-loader/webpack",
           "awesome-typescript-loader"
         ]
       },
     ]
-  },
-  externals: [
-    {
-      react: 'React',
-      'react-dom': 'ReactDOM',
-      'material-ui': 'MaterialUI',
-    }
-  ],
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-  ],
-
-  devServer: {
-    hot: true,
-    historyApiFallback: true,
   },
 };
